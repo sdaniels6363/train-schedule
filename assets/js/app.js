37e09b417a95fe5a9549f840228d43cf2eabba64
Scott Daniels <sdaniels6363@gmail.com>
2019-08-31 20:49:56 -0400

    The train ticker is successfully showing the next five arrivals from the database of trains.

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,63 +15,88 @@ $(document).ready(function () {
 
 
 
-  function trainTicker(){
+  function loadTrainsFromFirebase() {
+    // create empty train array
+
     trainArray = [];
-    // clear existing entries from ticker
-    $("#train-1").empty();
-    $("#train-2").empty();
-    $("#train-3").empty();
-    $("#train-4").empty();
-    $("#train-5").empty();
 
     database.ref().on("child_added", function (snapshot) {
       // storing the snapshot.val() in a variable for convenience
       var sv = snapshot.val();
-      console.log(sv);
+      // console.log(sv);
       test = sv;
       // determine train frequency
       var trainFreq = sv.trainInterval;
       var trainStart = moment(sv.trainServStart, "HH:mm").subtract(1, "years");
       var difference = moment().diff(trainStart, "minutes")
       var timeRemaining = trainFreq - (difference % trainFreq);
-  
-      trainArray.push({"trainFreq": sv.trainInterval,"trainLocation": sv.trainLocation, "timeRemaining":timeRemaining});
-      // Change the HTML to reflect the train arrivals
-      var arrival = `
-      <div>
-        <pre>
-  Train Name: ${sv.trainName}
-  City: ${sv.trainLocation}
-  Next Arrival: ${timeRemaining} minutes
-        </pre>
-      </div>
-      `
-  
-      if ($('#train-1').children().length === 0) {
-        $("#train-1").append(arrival);
-      } else if ($('#train-2').children().length === 0) {
-        $("#train-2").append(arrival);
-      } else if ($('#train-3').children().length === 0) {
-        $("#train-3").append(arrival);
-      } else if ($('#train-4').children().length === 0) {
-        $("#train-4").append(arrival);
-      } else if ($('#train-5').children().length === 0) {
-        $("#train-5").append(arrival);
+
+      trainArray.push({ "trainName": sv.trainName, "trainLocation": sv.trainLocation, "timeRemaining": timeRemaining })
+
+
+      // clear existing entries from ticker
+      $("#train-1").empty();
+      $("#train-2").empty();
+      $("#train-3").empty();
+      $("#train-4").empty();
+      $("#train-5").empty();
+
+      // sort the train array by timeRemaining
+      var sortedArray = trainArray.sort(function (a, b) {
+        return a.timeRemaining - b.timeRemaining;
+      });
+
+      function getTrainInfo(n) {
+        if (sortedArray[n]) {
+          // if the array entry exists get information.
+          var trainName = sortedArray[n].trainName;
+          var city = sortedArray[n].trainLocation;
+          var nextArrival = sortedArray[n].timeRemaining;
+
+          var arrival = `
+<div>
+  <pre>
+Train Name: ${trainName}
+City: ${city}
+Next Arrival: ${nextArrival} minutes
+  </pre>
+</div>
+                `
+          return arrival;
+
+        }
       }
+
+
+      train1 = getTrainInfo(0);
+      train2 = getTrainInfo(1);
+      train3 = getTrainInfo(2);
+      train4 = getTrainInfo(3);
+      train5 = getTrainInfo(4);
+
+      $("#train-1").append(train1);
+      $("#train-2").append(train2);
+      $("#train-3").append(train3);
+      $("#train-4").append(train4);
+      $("#train-5").append(train5);
+
+
       // Handle the errors
     }, function (errorObject) {
       console.log("Errors handled: " + errorObject.code);
     });
   };
 
+
+
   function startClock() {
     currTime = moment();
     $("#time-display").text("Current Time: " + currTime.format("HH:mm:ss"));
     var t = setTimeout(startClock, 500);
 
-    if (moment().seconds() === 0){
-      // update the ticker
-      trainTicker();
+    if (moment().seconds() === 0) {
+      // update train list every minute
+      loadTrainsFromFirebase();
     }
 
   };
@@ -96,11 +121,11 @@ $(document).ready(function () {
     $("#trainServStart").val("");
     $("#trainInterval").val("");
 
-    trainTicker();
-
+    loadTrainsFromFirebase(); // fetches the trains
   });
 
   startClock();
-  trainTicker();
+  loadTrainsFromFirebase(); // fetches the trains
+
 });
 
